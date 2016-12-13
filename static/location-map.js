72f8833ff8d1f990e2deb581be8367ab86b78530
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2016-12-13 12:25:26 +0100

    Minor code structure improvement.

--- static/location-map.js
+++ static/location-map.js
@@ -13,42 +13,44 @@ function initMap() {
 	var bounds = new google.maps.LatLngBounds();
 	$locations.each(function () {
 		var $location = $(this);
+		
 		var name = $location.data('name');
 		var lat = $location.data('lat');
 		var lng = $location.data('lng');
 		
-		if (lat && lng) {
-			var marker = new google.maps.Marker({
-				title: name,
-				position: {lat: lat, lng: lng},
-				// animation: google.maps.Animation.DROP,
-				map: map
-			});
-			
-			$location.hover(function () {
-				// TODO Zoom out until marker is visible.
-				marker.setAnimation(google.maps.Animation.BOUNCE);
-				$location.addClass('secondary');
-			}, function () {
-				marker.setAnimation(null);
-				$location.removeClass('secondary');
-			});
-			
-			marker.addListener('mouseover', function () {
-				$location.addClass('primary');
-				var locationElement = $location[0];
-				locationElement.scrollIntoView && locationElement.scrollIntoView({
-					behavior: 'smooth'
-				});
-			});
-			marker.addListener('mouseout', function () {
-				$location.removeClass('primary');
-			});
-			
-			bounds.extend(marker.getPosition());
-		} else {
+		if (!lat || !lng) {
 			$location.addClass('warning').attr('title', 'Could not find coordinates for this location...');
+			return;
 		}
+		
+		var marker = new google.maps.Marker({
+			title: name,
+			position: {lat: lat, lng: lng},
+			// animation: google.maps.Animation.DROP,
+			map: map
+		});
+		
+		$location.hover(function () {
+			// TODO Zoom out until marker is visible.
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+			$location.addClass('secondary');
+		}, function () {
+			marker.setAnimation(null);
+			$location.removeClass('secondary');
+		});
+		
+		marker.addListener('mouseover', function () {
+			$location.addClass('primary');
+			var locationElement = $location[0];
+			locationElement.scrollIntoView && locationElement.scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+		marker.addListener('mouseout', function () {
+			$location.removeClass('primary');
+		});
+		
+		bounds.extend(marker.getPosition());
 	});
 	
 	if (!bounds.isEmpty()) {
